2c255d864ec16a9a0ea5e100c35b550593e33ccc
skmin1993 <skmin95@gmail.com>
2021-04-05 17:40:41 +0800

    push

--- config/routes.rb
+++ config/routes.rb
@@ -8,23 +8,18 @@ Rails.application.routes.draw do
 
 
 
-
  resources :welcome, only: [:index] do
      collection do
       get :my
     end
 end
 
-  root  'welcome#my', as: nil, :constraints => DomainConstraint.new('anran.com.my')
-  root  'welcome#my', as: nil, :constraints => DomainConstraint.new('http://anran.com.my/')
-  root  'welcome#my', as: nil, :constraints => DomainConstraint.new('mydomain.com')
-  root  'welcome#my', as: nil, :constraints => DomainConstraint.new('localhost:3000')
-  root  'welcome#my', as: nil, :constraints => DomainConstraint.new('http://localhost:3000/')
-if Rails.env.include?('anran.com.my')
-    root 'welcome#my'
-elsif Rails.env.include?('an')
-    root 'welcome#my'
-else
-    root 'welcome#index'
-  end
+  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('anran.com.my')
+  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('http://anran.com.my/')
+  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('mydomain.com')
+  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('localhost:3000')
+  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('http://localhost:3000/')
+
+  root 'welcome#my'
+
 end
