8e34d229ef5d7da780829c0d556a234de8315c11
skmin1993 <skmin95@gmail.com>
2021-04-05 16:54:15 +0800

    git push

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 
 Rails.application.routes.draw do
-  get 'welcome/index'
+
  
-  root 'welcome#index'
+
+  constraints DomainConstraint.new('anran.com.my') do
+    root :to => 'welcome#my' , as: nil
+end
+
+    get 'welcome/index' 
+   root 'welcome#index' 
+
+
 
  resources :welcome, only: [:index] do
      collection do
       get :my
     end
-  end
-
+end
 end
