d1697f7f6a98497d31b7659f4ddc25a766c4df8d
skmin1993 <skmin95@gmail.com>
2021-04-05 17:43:42 +0800

    g

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,8 @@ Rails.application.routes.draw do
 end
 
   root  'welcome#index', as: nil, :constraints => DomainConstraint.new('anran.com.my')
-  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('http://anran.com.my/')
-  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('mydomain.com')
-  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('localhost:3000')
-  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('http://localhost:3000/')
+
+
 
   root 'welcome#my'
 
