c715c11a978664b7a7b45734e58331115fca38f5
skmin1993 <skmin95@gmail.com>
2021-04-06 11:06:01 +0800

    git push

---
+++ app/controllers/viennahome_controller.rb
@@ -0,0 +1,6 @@
+class ViennahomeController < ActionController::Base
+
+def index
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,19 @@
-
+require 'domain_constraint'
 Rails.application.routes.draw do
+  resources :welcome, only: [:index]
+  resources :viennahome, only: [:index]
+  constraints (DomainConstraint.new('anran.com.my')) do
+    root :to => 'welcome#index', as: nil
+      resources :welcome, only: [:index] do
+       collection do
+        get :my
+        end
+      end
+  end
 
- 
+  constraints(DomainConstraint.new('admin.api.project.dev')) do
+    root :to => 'welcome#index', as: nil
+  end
 
-
-    get 'welcome/index' 
-
-
-
- resources :welcome, only: [:index] do
-     collection do
-      get :my
-    end
-end
-
-  root  'welcome#index', as: nil, :constraints => DomainConstraint.new('anran.com.my')
-
-
-
-  root 'welcome#my'
-
+  root :to => 'viennahome#index', as: nil
 end
