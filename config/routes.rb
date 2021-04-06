c63910636f0b42a03b1e66fdd4737d95a60a317d
skmin1993 <skmin95@gmail.com>
2021-04-06 12:34:00 +0800

    git push

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,21 @@ Rails.application.routes.draw do
     root :to => 'viennahome#index', as: nil
   end
 
+ constraints(DomainConstraint.new('chinsan.com')) do
+    root :to => 'chinsan#index', as: nil
+    resources :product, only: [:index] do  
+    collection do
+      get :chicken_feed
+      get :duck_feed
+      get :cat_feed
+      get :dog_feed
+      get :cow_feed
+      get :fish_feed
+      get :rabbit_feed
+    end
+  end
+  end
+
+
   root :to => 'viennahome#index', as: nil
 end
