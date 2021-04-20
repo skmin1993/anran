4f1a0cfe0f264433caaab84ea375897d8a95c128
skmin1993 <skmin95@gmail.com>
2021-04-20 13:23:28 +0800

    git push

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,15 @@ Rails.application.routes.draw do
       end
   end
 
+  constraints (DomainConstraint.new('www.anran.com.my')) do
+    root :to => 'anran#index', as: nil
+      resources :anran, only: [:index] do
+       collection do
+        get :my
+        end
+      end
+  end
+
   constraints(DomainConstraint.new('bctherbalists.com.my')) do
     root :to => 'bctherbal#index', as: nil
       resources :bctherbal, only: [:index] do
@@ -20,8 +29,22 @@ Rails.application.routes.draw do
       end
   end
 
+  constraints(DomainConstraint.new('www.bctherbalists.com.my')) do
+    root :to => 'bctherbal#index', as: nil
+      resources :bctherbal, only: [:index] do
+       collection do
+        end
+      end
+      resources :payment, only: [:index] do  
+      end
+  end
 
-  constraints(DomainConstraint.new('viennahome.com.my' && 'www.viennahome.com.my')) do
+
+  constraints(DomainConstraint.new('viennahome.com.my')) do
+    root :to => 'viennahome#index', as: nil
+  end
+
+  constraints(DomainConstraint.new('www.viennahome.com.my')) do
     root :to => 'viennahome#index', as: nil
   end
 
@@ -29,6 +52,9 @@ Rails.application.routes.draw do
     root :to => 'iconmaker#index', as: nil
   end  
 
+  constraints(DomainConstraint.new('www.iconmakergroup.com.my')) do
+    root :to => 'iconmaker#index', as: nil
+  end
 
   constraints(DomainConstraint.new('yoshiyoshirobata.com.my')) do
     root :to => 'yoshirobata#index', as: nil
@@ -49,7 +75,45 @@ Rails.application.routes.draw do
     end
   end  
 
+  constraints(DomainConstraint.new('www.yoshiyoshirobata.com.my')) do
+    root :to => 'yoshirobata#index', as: nil
+    resources :cn, only: [:index] do
+    end
+
+    resources :menu, only: [:index] do
+       collection do
+        get :cn
+      end
+    end 
+
+
+   resources :recruit, only: [:index] do
+       collection do
+        get :cn
+      end
+    end
+  end  
+
   constraints(DomainConstraint.new('chinsan.my')) do
+    root :to => 'chinsan#index', as: nil
+     resources :chinsan, only: [:index] do
+       collection do
+        end
+      end
+    resources :animal, only: [:index] do  
+      collection do
+        get :chicken_feed
+        get :duck_feed
+        get :cat_feed
+        get :dog_feed
+        get :cow_feed
+        get :fish_feed
+        get :rabbit_feed
+      end
+    end
+  end
+
+  constraints(DomainConstraint.new('www.chinsan.my')) do
     root :to => 'chinsan#index', as: nil
      resources :chinsan, only: [:index] do
        collection do
