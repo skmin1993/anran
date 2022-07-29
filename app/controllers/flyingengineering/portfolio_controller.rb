28b9f5e1582ef90c76c939dbc24ff2a3d9246f5c
skmin1993 <skmin95@gmail.com>
2022-07-30 01:17:47 +0800

    flying engineering

---
+++ app/controllers/flyingengineering/portfolio_controller.rb
@@ -0,0 +1,111 @@
+class Flyingengineering::PortfolioController < ActionController::Base
+
+  def awning
+  end
+  
+  def awning_structure_fabrication
+  end
+  
+  def cage_fabrication
+  end
+  
+  def cabinet_fabrication
+  end
+  
+  def concrete_kerb
+  end
+  
+  def concrete_plinth
+  end
+  
+  def concrete_work
+  end
+  
+  def convenyor
+  end
+  
+  def custom_awning
+  end
+  
+  def custom_racking
+  end
+  
+  def custom_sink
+  end
+  
+  def custom_stainless_steel
+  end
+  
+  def custom_steel_work
+  end
+  
+  def drain_cover
+  end
+  
+  def fencing
+  end
+  
+  def frame_fabrication
+  end
+  
+  def guardhouse_barrier
+  end
+  
+  def gutter_fabrication
+  end
+  
+  def door_frame
+  end
+  
+  def truss
+  end
+  
+  def monkey_zigzag
+  end
+  
+  def others
+  end
+  
+  def piping_work
+  end
+  
+  def platform
+  end
+  
+  def pvc_curtain
+  end
+  
+  def ramp_fabrication
+  end
+  
+  def roofing_repair
+  end
+  
+  def safety_barrier_fabrication
+  end
+  
+  def safety_cover
+  end
+  
+  def stainless_steel_ducting
+  end
+  
+  def tank_fabrication
+  end
+  
+  def trolley_fabrication
+  end
+  
+  def trolley_track_repair
+  end
+  
+  def walk_way
+  end
+  
+  def waste_bin_fabrication
+  end
+  
+
+
+
+end
