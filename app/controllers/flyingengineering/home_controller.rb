4a4e7f144edd956487f0488466b358546c60e026
skmin1993 <skmin95@gmail.com>
2022-07-30 01:17:47 +0800

    flying engineering

---
+++ app/controllers/flyingengineering/home_controller.rb
@@ -0,0 +1,7 @@
+class Flyingengineering::HomeController < ActionController::Base
+
+  def index
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -162,34 +162,104 @@ Rails.application.routes.draw do
     end
   end
 
-
-    resources :iconmaker, only: [:index] do
-        collection do
-          get :about
+  constraints(DomainConstraint.new('flyingengineering.com')) do
+    root :to => 'flyingengineering/home#index', as: nil
+     namespace :flyingengineering, only: [:index] do
+       resources :home, only: [:index] do
+          collection do 
+            get :cn
+          end
         end
-    end
-
-   
-  root :to => 'yoshirobata/home#index', as: nil
-    namespace :yoshirobata do
-      resources :home, only: [:index] do
-        collection do 
-          get :cn
+      resources :portfolio, only: [:index] do
+          collection do
+            get 'awning'
+            get 'awning_structure_fabrication'
+            get 'cage_fabrication'
+            get 'cabinet_fabrication'
+            get 'concrete_kerb'
+            get 'concrete_plinth'
+            get 'concrete_work'
+            get 'convenyor'
+            get 'custom_awning'
+            get 'custom_racking'
+            get 'custom_sink'
+            get 'custom_stainless_steel'
+            get 'custom_steel_work'
+            get 'drain_cover'
+            get 'fencing'
+            get 'frame_fabrication'
+            get 'guardhouse_barrier'
+            get 'gutter_fabrication'
+            get 'door_frame'
+            get 'truss'
+            get 'monkey_zigzag'
+            get 'others'
+            get 'piping_work'
+            get 'platform'
+            get 'pvc_curtain'
+            get 'ramp_fabrication'
+            get 'roofing_repair'
+            get 'safety_barrier_fabrication'
+            get 'safety_cover'
+            get 'stainless_steel_ducting'
+            get 'tank_fabrication'
+            get 'trolley_fabrication'
+            get 'trolley_track_repair'
+            get 'walk_way'
+            get 'waste_bin_fabrication'
+          end
         end
-    end
-    resources :menu, only: [:index] do
-       collection do
-        get :cn
       end
-    end 
-      resources :recruit, only: [:index] do
-       collection do
-        get :cn
-      end
-    end
   end
 
-
-
+  constraints(DomainConstraint.new('www.flyingengineering.com')) do
+    root :to => 'flyingengineering/home#index', as: nil
+     namespace :flyingengineering, only: [:index] do
+       resources :home, only: [:index] do
+          collection do 
+            get :cn
+          end
+        end
+      resources :portfolio, only: [:index] do
+          collection do
+            get 'awning'
+            get 'awning_structure_fabrication'
+            get 'cage_fabrication'
+            get 'cabinet_fabrication'
+            get 'concrete_kerb'
+            get 'concrete_plinth'
+            get 'concrete_work'
+            get 'convenyor'
+            get 'custom_awning'
+            get 'custom_racking'
+            get 'custom_sink'
+            get 'custom_stainless_steel'
+            get 'custom_steel_work'
+            get 'drain_cover'
+            get 'fencing'
+            get 'frame_fabrication'
+            get 'guardhouse_barrier'
+            get 'gutter_fabrication'
+            get 'door_frame'
+            get 'truss'
+            get 'monkey_zigzag'
+            get 'others'
+            get 'piping_work'
+            get 'platform'
+            get 'pvc_curtain'
+            get 'ramp_fabrication'
+            get 'roofing_repair'
+            get 'safety_barrier_fabrication'
+            get 'safety_cover'
+            get 'stainless_steel_ducting'
+            get 'tank_fabrication'
+            get 'trolley_fabrication'
+            get 'trolley_track_repair'
+            get 'walk_way'
+            get 'waste_bin_fabrication'
+          end
+        end
+      end
+  end
   
 end
