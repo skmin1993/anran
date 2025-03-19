f1227fc573c20704f22d45f242a193054c23b169
skmin1993 <51898595+skmin1993@users.noreply.github.com>
2025-03-19 10:41:26 +0800

    fix error

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.config.hosts << "flyingengineering.com"
+Rails.application.config.hosts << "www.flyingengineering.com"
